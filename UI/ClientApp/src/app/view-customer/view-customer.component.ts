3ff67bc8360a379e0440a5430e86b01dcd0802e7
Shadman Kudchikar <shadmanssk@yahoo.in>
2020-02-23 14:58:43 +0530

    add view customer component logic

--- UI/ClientApp/src/app/view-customer/view-customer.component.ts
+++ UI/ClientApp/src/app/view-customer/view-customer.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../services/customer.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-view-customer',
-  templateUrl: './view-customer.component.html',
-  styleUrls: ['./view-customer.component.css']
+    selector: 'app-view-customer',
+    templateUrl: './view-customer.component.html',
+    styleUrls: ['./view-customer.component.css']
 })
 export class ViewCustomerComponent implements OnInit {
+    customer = { addresses: [] };
+    address = {};
+    constructor(private customerService: CustomerService,
+        private route: ActivatedRoute) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.customerService.get(this.route.snapshot.params.id)
+            .subscribe((data) => {
+                this.customer = data;
+            }, (err) => {
+                alert("Failed to load customer details");
+                console.log(err);
+            });
+    }
 
 }
