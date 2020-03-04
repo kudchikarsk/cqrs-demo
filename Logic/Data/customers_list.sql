8781d25514510a52aa8c686945028f7826d6379c
Shadman Kudchikar <shadmanssk@yahoo.in>
2020-03-04 21:30:25 +0530

    add sql for reading

---
+++ Logic/Data/customers_list.sql
@@ -0,0 +1,8 @@
+﻿SELECT * FROM 
+Customers INNER JOIN Addresses
+ON Addresses.Id = (
+        SELECT  TOP 1 Id
+        FROM    Addresses
+		WHERE CustomerId = CustomerId
+        ORDER BY  IsPrimary DESC
+         );
